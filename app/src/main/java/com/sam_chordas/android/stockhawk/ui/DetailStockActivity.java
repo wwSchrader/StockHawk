438c79e6f6656ac61248cc6900c77a7314e38bc1
Warren Schrader <wwschrader@gmail.com>
2016-07-18 22:18:48 -0700

    Add activity for stock detail screen.

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/DetailStockActivity.java
@@ -0,0 +1,11 @@
+package com.sam_chordas.android.stockhawk.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Warren on 7/18/2016.
+ */
+public class DetailStockActivity extends AppCompatActivity{
+
+
+}
